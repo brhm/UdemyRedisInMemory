cddce1dbe5a3bf08d8f9bcc5bf83426d169d8740
ibrahim Keskin <brhmkskn04@gmail.com>
2021-05-07 22:20:24 +0300

    TryGetvalue remove

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -18,12 +18,36 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Index()
         {
 
-            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            // 1. yol değer zaman adında parametre var mı yokmu kontol ediyoruz
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //2. yol
+
+            if (!_memoryCache.TryGetValue("zaman",out string zamancache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
 
             return View();
         }
         public IActionResult Show()
         {
+
+            //cache silme işlemi.
+            _memoryCache.Remove("zaman");
+
+            //kontrol et varsa getir yoksa oluşturup değer set et
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                
+                return DateTime.Now.ToString();
+            });
+
+
 
             ViewBag.Zaman= _memoryCache.Get<string>("zaman");
 
