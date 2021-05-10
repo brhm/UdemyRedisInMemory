b05b59c4606853414244fa684ee677007723a344
ibrahim Keskin <brhmkskn04@gmail.com>
2021-05-10 17:45:14 +0300

    redis serialize and byte cache

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
@@ -24,6 +27,20 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache.SetString("name", "ibrahim", options);
            await  _distributedCache.SetStringAsync("surname", "keskin", options);
 
+            Product product = new Product() { Id = 1, Name = "Kalem", Price = 15.9M, Stock = 10 };
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            await _distributedCache.SetStringAsync("product:1", jsonProduct,options);
+
+            Product product2 = new Product() { Id = 2, Name = "Kalem2", Price = 19.9M, Stock = 10 };
+
+            string jsonProduct2 = JsonConvert.SerializeObject(product2);
+
+            Byte[] productByte = Encoding.UTF8.GetBytes(jsonProduct2);
+            await _distributedCache.SetAsync("product:2", productByte, options);
+
+
             return View();
         }
 
@@ -31,8 +48,22 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             var name=_distributedCache.GetString("name");
             var surname=await _distributedCache.GetStringAsync("surname");
+
             ViewBag.Name = name;
             ViewBag.Surname = surname;
+
+
+
+            var productJson=await _distributedCache.GetStringAsync("product:1");
+            Product product = JsonConvert.DeserializeObject<Product>(productJson);
+            ViewBag.Product = product;
+
+
+            var productByte =  await _distributedCache.GetAsync("product:2");
+
+            string productJson2 = Encoding.UTF8.GetString(productByte);
+            Product product2 = JsonConvert.DeserializeObject<Product>(productJson2);
+            ViewBag.Product2 = product2;
 
             return View();
         }
