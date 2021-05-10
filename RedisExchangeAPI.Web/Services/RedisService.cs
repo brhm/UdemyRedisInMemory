d63ff2f6b182528fbcc1a1e17936ced8aa335cb4
ibrahim Keskin <brhmkskn04@gmail.com>
2021-05-10 21:57:32 +0300

    create project and redis service

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        public IDatabase db { get; set; }
+        private ConnectionMultiplexer _redis;
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+        }
+        /// <summary>
+        /// redis server üzerinden bulunan dblerden birini alıyoruz. Redis desktop manager da 16 tane db görünüyor.
+        /// </summary>
+        /// <returns></returns>
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
