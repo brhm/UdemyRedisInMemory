dbcb09ebff2ee72c0f0167431d2d46a8c83f9a9a
ibrahim Keskin <brhmkskn04@gmail.com>
2021-05-11 23:55:04 +0300

    hashtype dictionary

---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+
+        private string setKey = "sozluk";      
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> nameList = new Dictionary<string, string>();
+
+            if (db.KeyExists(setKey))
+            {
+                //db.SortedSetScan(setKey).ToList().ForEach(x =>
+                //{
+                //    //x.Element
+                //    //x.Score
+                //    nameList.Add(x.Element.ToString());
+                //});
+
+                db.HashGetAll(setKey).ToList().ForEach(x =>
+                {
+                    nameList.Add(x.Name,x.Value);
+                });
+
+            }
+
+            return View(nameList);
+        }
+        public IActionResult Add(string name, string value)
+        {
+            if (!db.KeyExists(setKey))
+            {
+                db.KeyExpire(setKey, DateTime.Now.AddMinutes(1));
+            }
+            db.HashSet(setKey, name,value);
+
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(string name)
+        {
+            await db.HashDeleteAsync(setKey, name);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
